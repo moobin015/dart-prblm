baa98259add8168b5202bf005f35b22a5a9c7503
moobin <moobinislam3@gmail.com>
2024-06-17 12:27:53 +0600

    a

---
+++ oop/class_clone.dart
@@ -0,0 +1,11 @@
+import 'inheritance.dart';
+
+
+
+void main() {
+  Animal animal = Animal("Horse");
+  animal.Bird();
+  //
+  Cow cow =Cow('Tiya');
+  cow.Bird();
+}
--- oop/inheritance.dart
+++ oop/inheritance.dart
@@ -21,10 +21,3 @@ class Cow extends Animal {
   }
 }
 
-void main() {
-  Animal animal = Animal("Horse");
-  animal.Bird();
-  //
-  Cow cow =Cow('Tiya');
-  cow.Bird();
-}
