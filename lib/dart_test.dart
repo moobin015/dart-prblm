f751c3f1db5c0727649857583f899aa48b61750e
moobin-ubuntu <moobinislam3@gmail.com>
2024-09-19 21:00:55 +0600

    cc

--- lib/dart_test.dart
+++ lib/dart_test.dart
@@ -1,3 +1,17 @@
-int calculate() {
-  return 6 * 7;
+void main() {
+  myMassage('Moobin');
+
+  /*
+  * inter yoru name
+  * inter your valu
+  * /
+  myMassage('Roni');
+
+   */
+}
+
+void myMassage(String name) {
+  print('Welcome to Our Company $name');
+  print('Mrs. $name plesle Puse your IDE Card');
+  print('Thanks $name');
 }
